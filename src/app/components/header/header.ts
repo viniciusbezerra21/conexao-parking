793805c1842be8c3cda163779dae604a9cb2fa44
Vinicius.Bezerra <viniciusg21bezerra@gmail.com>
2026-03-04 11:23:36 -0300

    funcionalidade de sair da pagina adicionada

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,33 @@ import { Router } from '@angular/router';
   styleUrl: './header.css',
 })
 export class Header {
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private eRef: ElementRef
+  ) { }
 
+  
   irParaDashboard() {
     this.router.navigate(['dashboard']);
   }
   
+  menuAberto = false;
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['']);
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.menuAberto = false;
+    }
+  }
+
 }
--- src/app/services/services/auth.service.ts
+++ src/app/services/services/auth.service.ts
@@ -20,5 +20,8 @@ export class AuthService {
     );
   }
 
+  logout() {
+    localStorage.removeItem('token');
+  }
   
 }
